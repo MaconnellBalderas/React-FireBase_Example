8fd0210264f662e130b8025e76561e0344e27814
Maconnell <maconnell.balderas@gmail.com>
2023-10-24 21:13:54 -0500

    Added Firebase config

---
+++ config/firebase-config.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCqyC7vZBgu4sI-9AZxEpK7g75N33ectB4",
+  authDomain: "react-firebase-app-d723a.firebaseapp.com",
+  projectId: "react-firebase-app-d723a",
+  storageBucket: "react-firebase-app-d723a.appspot.com",
+  messagingSenderId: "586174526984",
+  appId: "1:586174526984:web:7e384a7d501df21d9203bd"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
